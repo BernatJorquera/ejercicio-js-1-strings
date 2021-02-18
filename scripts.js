613847d83468688abbd820216f82b1e4228c380f
Bernat Jorquera <bernatjv19@gmail.com>
2021-02-18 20:00:50 +0100

    First commit

---
+++ scripts.js
@@ -0,0 +1,3 @@
+let palabraMinus = "supercalifragilisticospialidoso";
+let palabraMayusPrimeraLetra = palabraMinus.charAt(0).toUpperCase() + palabraMinus.slice(1)
+console.log(palabraMayusPrimeraLetra);
